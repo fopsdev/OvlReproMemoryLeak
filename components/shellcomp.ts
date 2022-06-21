ed77e108901a1909bcbaf85561e8ae6f67831ea0
fops <fopsdev@gmail.com>
2022-06-21 09:41:42 +0200

    wip

--- components/shellcomp.ts
+++ components/shellcomp.ts
@@ -3,33 +3,21 @@ import { OvlBaseElement } from "../OvlBaseElement"
 
 export class ShellComp extends OvlBaseElement {
   comp1Visible: boolean
-  comp1ShowDurationMs: number
   getUI() {
     let comp1
     if (this.comp1Visible) {
       comp1 = html`<ovl-comp1></ovl-comp1>`
     }
     return html`<div>
-      <div>Shell</div>
+      <button
+        @click=${(e) => {
+          this.comp1Visible = !this.comp1Visible
+          this.doRender()
+        }}
+      >
+        Toggle Comp1
+      </button>
       ${comp1}
     </div>`
-  }
-  connectedCallback() {
-    this.comp1ShowDurationMs = 7000
-    setTimeout(() => {
-      this.comp1Toggler()
-    }, this.comp1ShowDurationMs)
-    super.connectedCallback()
-  }
-  comp1Toggler() {
-    this.comp1ShowDurationMs = this.comp1ShowDurationMs / 2
-    if (this.comp1ShowDurationMs < 1000) {
-      this.comp1ShowDurationMs = 1000
-    }
-    this.comp1Visible = !this.comp1Visible
-    this.doRender()
-    setTimeout(() => {
-      this.comp1Toggler()
-    }, this.comp1ShowDurationMs)
   }
 }
