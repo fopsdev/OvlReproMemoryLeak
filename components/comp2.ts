d35a68bea14e913064703aa70a88aad43492ea64
fops <fopsdev@gmail.com>
2022-06-25 10:37:21 +0200

    wip

---
+++ components/comp2.ts
@@ -0,0 +1,15 @@
+import { html } from "lit-html"
+import { OvlBaseElement } from "../OvlBaseElement"
+
+export class Comp2 extends OvlBaseElement {
+  getUI() {
+    // also add a few handlers to make the problem more obvious in e.g. chrome memory profiler
+    return html`<h1
+      @click=${(e) => console.log("clicked")}
+      @mousedown=${(e) => console.log("mousedown")}
+      @mouseup=${(e) => console.log("mouseup")}
+    >
+      Comp2
+    </h1>`
+  }
+}
--- components/shellcomp.ts
+++ components/shellcomp.ts
@@ -1,23 +1,53 @@
-import { html } from "lit-html"
+import { html, render } from "lit-html"
 import { OvlBaseElement } from "../OvlBaseElement"
 
 export class ShellComp extends OvlBaseElement {
-  comp1Visible: boolean
+  compVisible: boolean
+  activeComp: "Comp1" | "Comp2"
   getUI() {
-    let comp1
-    if (this.comp1Visible) {
-      comp1 = html`<ovl-comp1></ovl-comp1>`
+    let comp
+    if (this.compVisible) {
+      if (this.activeComp === "Comp1") {
+        comp = html`<ovl-comp1></ovl-comp1>`
+      } else {
+        comp = html`<ovl-comp2></ovl-comp2>`
+      }
     }
     return html`<div>
       <button
         @click=${(e) => {
-          this.comp1Visible = !this.comp1Visible
+          this.compVisible = !this.compVisible
           this.doRender()
         }}
       >
-        Toggle Comp1
+        Toggle Visible
       </button>
-      ${comp1}
+      <button
+        @click=${(e) => {
+          if (this.activeComp === "Comp1") {
+            this.activeComp = "Comp2"
+          } else {
+            this.activeComp = "Comp1"
+          }
+          this.doRender()
+        }}
+      >
+        Toggle Comp
+      </button>
+      <button
+        @click=${(e) => {
+          setTimeout(() => {
+            render(undefined, document.getElementById("app"))
+          }, 0)
+        }}
+      >
+        Cleanup Shell
+      </button>
+      <div>Active Comp: ${this.activeComp} ${comp}</div>
     </div>`
+  }
+  connectedCallback() {
+    this.activeComp = "Comp1"
+    super.connectedCallback()
   }
 }
